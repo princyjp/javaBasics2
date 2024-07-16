5791070736633557205dfe681d585c73cb4fadb1
princyjp <princy.philip@outlook.com>
2024-07-16 16:36:00 +0100

    Feat: second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String name = "Hello Princy";
+        int birthYear = 1995;
+        double pi = 3.14;
+        boolean isLearningJava= true;
+        System.out.println(name);
+        System.out.println("I was born in" + birthYear);
+        System.out.println(isLearningJava);
+        String favouriteFilm = "Thor";
+        System.out.println("My favourite film is "+ favouriteFilm);
+        favouriteFilm = "Hannah Montana";
+        System.out.println("My favourite film is "+ favouriteFilm);
+
     }
 }
